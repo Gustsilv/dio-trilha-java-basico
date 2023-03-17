1f8f381d078e8d1992cc86ff2219f80274e2cac7
Gustavo  Alves da Silva <gustavoalves.silva1994@gmail.com>
2023-03-17 16:10:59 +0000

    Começo da execução de um exercício sobre fluxos no java

---
+++ Operadores-lógicos/src/Fluxos.java
@@ -0,0 +1,13 @@
+public class Fluxos {
+    public static void main(String[] args) {
+        ifFlecha();
+        ifSemflecha();
+        ifFerias();
+        ifMenor();
+
+        switchSemana();
+        switchNumero();
+        switchFerias();
+    }
+    private static void ifFlecha
+}
